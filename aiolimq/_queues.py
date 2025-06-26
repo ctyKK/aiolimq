2c4cf8accd41862847a96e12cb933b369cc90fae
ctyKK <ctyKK@outlook.com>
2025-06-27 04:34:18 +0800

    _queues remove-change - remove LimitedQueue.shutdown - remove LimitedQueue._limiter_closed - change docstring

--- aiolimq/_queues.py
+++ aiolimq/_queues.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 import heapq
-from asyncio import CancelledError, Queue
+from asyncio import Queue
 from typing import Generic
 
 from asynciolimiter import _CommonLimiterMixin
@@ -12,7 +12,7 @@ from ._types import L, P, S, V
 
 
 class LimitedQueue(Queue[V], Generic[L, V]):
-    """Limited asyncio.Queue."""
+    """Limited asyncio.Queue"""
 
     def __init__(self, limiter: L, maxsize: int = 0) -> None:
         super().__init__(maxsize=maxsize)
@@ -21,32 +21,23 @@ class LimitedQueue(Queue[V], Generic[L, V]):
             raise TypeError(type(limiter).__name__)
 
         self._limiter = limiter
-        self._limiter_closed: bool = False
 
     async def put(self, item: V) -> None:
         return await super().put(item)
 
     async def get(self) -> V:
-        try:
-            await self._limiter.wait()
-        except CancelledError:
-            if self._limiter_closed:
-                raise
+        await self._limiter.wait()
         return await super().get()
-
-    # def shutdown(self, immediate: bool = False) -> None:
-    #     # TODO: added in python-3.13
-    #     self._limiter.close()
-    #     super().shutdown(immediate=immediate)
 
 
 class LimitedPriorityQueue(LimitedQueue[L, V], Generic[L, S, V]):
-    """Limited priority asyncio.Queue."""
+    """Limited priority asyncio.Queue"""
 
     def __init__(self, limiter: L, priority: P[V, S] = lambda _: _, maxsize: int = 0) -> None:  # type: ignore
         super().__init__(limiter, maxsize)
 
         self._priority: P[V, S] = priority
+        """Priority calculator"""
         self._auto_id: int = 0
         """Auto increment ID"""
 
