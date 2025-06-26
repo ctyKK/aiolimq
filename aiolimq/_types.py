8a21bc5eafdc2eddc7b273f7d9b2da7700b9781a
ctyKK <ctyKK@outlook.com>
2025-06-27 00:17:00 +0800

    aiolimq._types add custom-types - V - L - SupportsGtLt

--- aiolimq/_types.py
+++ aiolimq/_types.py
@@ -1,1 +1,22 @@
 """Type-hint"""
+
+from __future__ import annotations
+
+from typing import Protocol, TypeVar, runtime_checkable
+
+from asynciolimiter import _CommonLimiterMixin
+
+
+V = TypeVar('V')
+
+L = TypeVar('L', bound=_CommonLimiterMixin)
+"""Limiter Type"""
+
+
+@runtime_checkable
+class SupportsGtLt(Protocol):
+    def __gt__(self, other, /) -> bool: ...
+    def __lt__(self, other, /) -> bool: ...
+
+
+SupportsGtLtV = TypeVar('SupportsGtLtV', bound=SupportsGtLt)
