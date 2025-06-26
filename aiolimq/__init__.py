d851d6fe4d846a63f650789ad603a102b7e5bb16
ctyKK <ctyKK@outlook.com>
2025-06-27 04:19:04 +0800

    aiolimq remove-change - remove _limiters - import LeakyBucketLimiter, Limiter, StrictLimiter from asynciolimiter

--- aiolimq/__init__.py
+++ aiolimq/__init__.py
@@ -3,5 +3,5 @@
 Asyncio rate-limited queues for Python.
 """
 
-from ._limiters import LeakyBucketLimiter, Limiter, StrictLimiter
+from asynciolimiter import LeakyBucketLimiter, Limiter, StrictLimiter
 from ._queues import LimitedQueue, LimitedPriorityQueue
