a9c65a7e942139a82f5cd0d8a8381bd212f24524
ctyKK <ctyKK@outlook.com>
2025-06-27 00:46:00 +0800

    aiolimq._types add-change - add docstring for SupportsGtLt, SupportsGtLtV - rename SupportsGtLtV->S - add P

--- aiolimq/_types.py
+++ aiolimq/_types.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from typing import Protocol, TypeVar, runtime_checkable
+from typing import Callable, Protocol, TypeVar, runtime_checkable
 
 from asynciolimiter import _CommonLimiterMixin
 
@@ -10,13 +10,19 @@ from asynciolimiter import _CommonLimiterMixin
 V = TypeVar('V')
 
 L = TypeVar('L', bound=_CommonLimiterMixin)
-"""Limiter Type"""
+"""Limiter Type."""
 
 
 @runtime_checkable
 class SupportsGtLt(Protocol):
+    """Support __gt__ and __lt__."""
+
     def __gt__(self, other, /) -> bool: ...
     def __lt__(self, other, /) -> bool: ...
 
 
-SupportsGtLtV = TypeVar('SupportsGtLtV', bound=SupportsGtLt)
+S = TypeVar('S', bound=SupportsGtLt)
+"""Object supports __gt__ and __lt__."""
+
+P = Callable[[V], S]
+"""Priority calculate method."""
